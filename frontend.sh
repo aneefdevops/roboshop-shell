21378257c503406bca1ad8cec74c7b1f09e8ad17
aneef <awss2423@gmail.com>
2023-08-15 15:25:22 +0530

    frontend.sh and roboshop.conf

---
+++ frontend.sh
@@ -0,0 +1,15 @@
+echo -e '\e[36m>>>>>install nginx<<<<<\e[0m'
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+echo -e '\e[36m>>>>>remove content <<<<<\e[0m'
+rm -rf /usr/share/nginx/html/*
+echo -e '\e[36m>>>>>dowmload frontend content <<<<<\e[0m'
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e '\e[36m>>>>> extract frontend content <<<<<\e[0m'
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+echo -e '\e[36m>>>>> add repo file <<<<<\e[0m'
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e '\e[36m>>>>> restat  <<<<<\e[0m'
+systemctl restart nginx
