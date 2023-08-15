2b3fe07ca04e46cbf69f527f5f8152076cf49861
aneef <awss2423@gmail.com>
2023-08-15 15:36:33 +0530

    mangodb.sh and mangodb.repo

--- mangodb.sh
+++ mangodb.sh
@@ -2,6 +2,7 @@ echo -e '\e[36m>>>>> add repo file <<<<<\e[0m'
 cp mangodb.repo /etc/yum.repos.d/mongo.repo
 echo -e '\e[36m>>>>> install mangodb <<<<<\e[0m'
 yum install mongodb-org -y
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 echo -e '\e[36m>>>>> start and enable <<<<<\e[0m'
 systemctl enable mongod
 systemctl start mongod
