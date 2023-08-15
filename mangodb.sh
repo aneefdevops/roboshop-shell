56b96bebd0a52b23a743791261a9109966c2cd1c
aneef <awss2423@gmail.com>
2023-08-15 19:18:14 +0530

    catalogue.sh and catalogue.service

--- mangodb.sh
+++ mangodb.sh
@@ -1,5 +1,5 @@
 echo -e '\e[36m>>>>> add repo file <<<<<\e[0m'
-cp /home/centos/roboshop-shell/ mangodb.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mangodb.repo /etc/yum.repos.d/mongo.repo
 echo -e '\e[36m>>>>> install mangodb <<<<<\e[0m'
 yum install mongodb-org -y
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
