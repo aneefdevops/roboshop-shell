b809ae9909dbff364eddd4b7a700b0301b41ba6d
aneef <awss2423@gmail.com>
2023-08-16 07:05:27 +0530

    catalogue.sh and catalogue.service

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+echo -e '\e[36m>>>>> disable my sql <<<<<\e[0m'
+yum module disable mysql -y
+echo -e '\e[36m>>>>> redis copy service file  <<<<<\e[0m'
+cp /home/centos/roboshop-shell/mysql.service /etc/yum.repos.d/mysql.repo
+echo -e '\e[36m>>>>> install mysql <<<<<\e[0m'
+yum install mysql-community-server -y
+echo -e '\e[36m>>>>> reload and restat   <<<<<\e[0m'
+systemctl enable mysqld
+systemctl restart mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
+mysql -uroot -pRoboShop@1
+
