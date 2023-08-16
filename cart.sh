613d1ad94003c0c2279e2b7eda28b99791587528
aneef <awss2423@gmail.com>
2023-08-16 06:47:27 +0530

    catalogue.sh and catalogue.service

---
+++ cart.sh
@@ -0,0 +1,20 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e '\e[36m>>>>> install nodejs <<<<<\e[0m'
+yum install nodejs -y
+echo -e '\e[36m>>>>> useradd  <<<<<\e[0m'
+useradd roboshop
+echo -e '\e[36m>>>>> unzip  <<<<<\e[0m'
+rm -rf /app
+mkdir /app
+echo -e '\e[36m>>>>> cart zip  <<<<<\e[0m'
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+unzip /tmp/cart.zip
+echo -e '\e[36m>>>>> nmp intall  <<<<<\e[0m'
+cd /app
+npm install
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+echo -e '\e[36m>>>>> start and reload <<<<<\e[0m'
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
