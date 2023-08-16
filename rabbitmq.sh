195f981ec804f5d4ff7e0d41297e92a3a385a4a2
aneef <awss2423@gmail.com>
2023-08-16 07:29:41 +0530

    shipping.sh and shipping.service

---
+++ rabbitmq.sh
@@ -0,0 +1,10 @@
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+echo -e '\e[36m>>>>> install server  <<<<<\e[0m'
+yum install rabbitmq-server -y
+echo -e '\e[36m>>>>> reload and stat   <<<<<\e[0m'
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+echo -e '\e[36m>>>>> add user   <<<<<\e[0m'
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
